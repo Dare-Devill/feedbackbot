174764e93a2a433c91078ab3bee073f5c556a8f8
Dare-Devill <70199827+Dare-Devill@users.noreply.github.com>
2020-12-13 11:02:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
       API_ID = int(os.environ.get("API_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      OWNER = [os.environ.get("OWNER_ID",  "")]
-      PASS = os.environ.get("PASSWORD", "")
+      API_HASH = os.environ.get("API_HASH" , "")
+      OWNER = [os.environ.get("OWNER_ID",  "@pratham_vai")]
+      PASS = os.environ.get("PASSWORD", "mishra")
       RULES = os.environ.get("RULES", "")
-      START = os.environ.get("START", "")
+      START = os.environ.get("START", "I am a feedback bot for Pratham. you can send feed back or ask for pm in this bot.")
       SEND = []
       LOGIN = []
       feedback = []
